9d20073f1d5ba96653d101a1d2a65f318a83e423
Anastasia Myropolska <myropolska@gmail.com>
2025-04-13 19:05:14 +0200

    move more code to view model

--- Architecture/ContentView.swift
+++ Architecture/ContentView.swift
@@ -12,19 +12,15 @@ struct ContentView: View {
 
 	var viewModel = ContentViewModel()
 
-	//private var locationManager = LocationManager()
 	@State private var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
-	@State private var visibleRegion: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.1967608, longitude: 11.4132661), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)) // has to be @State, otherwise not possible to change in onMapCameraChange // TODO: wft is this, why hardcoded? we should use user's location
-	// position.followsUserLocation = true
-
-	@State private var markers: [MKMapItem] = []
-	@State private var selectedResult: MKMapItem?
 
 	@State private var route: MKRoute?
 
+	@State private var selectedResult: MKMapItem? // move to viewModel
+
 	var body: some View {
 		Map(position: $cameraPosition, selection: $selectedResult) {
-			ForEach (markers, id: \.self) { marker in
+			ForEach (viewModel.visibleMarkers, id: \.self) { marker in
 				Marker(item: marker)
 			}
 
@@ -32,21 +28,21 @@ struct ContentView: View {
 		}
 		.task {
 			// guard markers.isEmpty else { return }
-			markers = await viewModel.requestArtifacts()
+			await viewModel.requestArtifacts()
 		}
 		.onMapCameraChange { context in
-			viewModel.visibleRegion = context.region // called many times, why?
+			viewModel.visibleRegion = context.region // called many times, why? is this why it's slow?
 
 			Task {
-				markers = await viewModel.requestArtifacts()
+				await viewModel.requestArtifacts()
 			}
 		}
 //		.onChange(of: selectedResult) {
 //			getDirection()
 //		}
 		.safeAreaInset(edge: .bottom) {
-			if let selectedResult {
-				ItemInfoView(selectedMarker: selectedResult, route: route)
+			if let selectedMarker = viewModel.selectedMarker {
+				ItemInfoView(selectedMarker: selectedMarker, route: route)
 					.frame(height: 128)
 					.clipShape(RoundedRectangle(cornerRadius: 10))
 					.padding([.top, .horizontal])
--- Architecture/ContentViewModel.swift
+++ Architecture/ContentViewModel.swift
@@ -8,13 +8,13 @@
 import MapKit
 import SwiftUICore
 
-class ContentViewModel {
+@Observable class ContentViewModel {
 	private var locationManager = LocationManager()
 	var visibleRegion: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.1967608, longitude: 11.4132661), span: MKCoordinateSpan(latitudeDelta: 15, longitudeDelta: 15)) // has to be @State, otherwise not possible to change in onMapCameraChange // TODO: wft is this, why hardcoded? we should use user's location
 	var visibleMarkers: [MKMapItem] = []//[ArtefactMapItem] = []
 	var selectedMarker: MKMapItem?//ArtefactMapItem?
 
-	func requestArtifacts() async -> [MKMapItem] {
+	func requestArtifacts() async {
 		let request = Request(region: visibleRegion)
 		do {
 			let artefacts = try await Parser(request: request).parse()
@@ -22,7 +22,5 @@ class ContentViewModel {
 		} catch {
 			visibleMarkers = []
 		}
-		return visibleMarkers
-
 	}
 }
