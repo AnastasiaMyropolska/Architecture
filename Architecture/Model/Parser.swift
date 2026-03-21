714e9aa9dcc1151a0837d9c56178452df2d6f059
Anastasia Myropolska <myropolska@gmail.com>
2026-03-21 14:33:00 +0100

    add remote request - works

--- Architecture/Model/Parser.swift
+++ Architecture/Model/Parser.swift
@@ -60,44 +60,60 @@ struct Parser {
 		let thema: String
 		let longitude: Double
 		let latitude: Double
-		let version: String
-		let lookAroundVersion: String
 		let precision: Double
+		let lookAroundVersion: String
+		let calltype: String
 	}
 
 	func parseRemote() async throws -> [Artefact] {
-		guard let url = URL(string: "http://94.130.181.51:8080/ArtefactsLocation-api/supplyArtefacts") else {
+		guard let url = URL(string: "https://apalladio.org/architecture-api/supplyArtefacts") else {
 			throw NetworkingError.resourceNotFound
 		}
+
 
 		let requestBody = ArtefactRequest(
 			token: "pk.eyJ1IjoibXlyb3BvbHNreWkiLCJhIjoiY2t1dHVibDZvMmZlNDJwcDFwMTI3ZjU0dyJ9.J_kmeuSJGKwoKiqQXVfFAQ",
 			thema: "Architecture",
 			longitude: request.region.center.longitude,
 			latitude: request.region.center.latitude,
-			version: "2.0.3",
+			precision: 0.1,
 			lookAroundVersion: "3.0.0",
-			precision: 0.05
+			calltype: "web"
 		)
 
 		let jsonData = try JSONEncoder().encode(requestBody)
 
-		guard let url1 = Bundle.main.url(forResource: "request", withExtension: "json") else {
-			throw NetworkingError.resourceNotFound
-		}
-		let (data1, responce1) = try await URLSession.shared.data(from: url1)
+//		guard let url1 = Bundle.main.url(forResource: "request", withExtension: "json") else {
+//			throw NetworkingError.resourceNotFound
+//		}
+		//let (data1, responce1) = try await URLSession.shared.data(from: url1)
 
 		// 3. Create URLRequest
 		var request = URLRequest(url: url)
 		request.httpMethod = "POST"
 		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-		request.httpBody = data1
+		request.httpBody = jsonData
 
 		let (data, response) = try await URLSession.shared.data(for: request)
 
 		// Optional: check HTTP status code
 		if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
 			throw URLError(.badServerResponse)
+		}
+
+		do {
+			let decodedResponse = try JSONDecoder().decode(ResponseData.self, from: data)
+			let artefacts = decodedResponse.artefacts
+			//let visibleArtefacts = artefacts.filter{ request.region.contains( $0.artefactsLocation.location) }
+			return artefacts
+		} catch let DecodingError.keyNotFound(key, context) {
+			print("Missing key: \(key), \(context)")
+		}
+		catch let DecodingError.typeMismatch(type, context) {
+			print("Type mismatch: \(type), \(context)")
+		}
+		catch {
+			print(error)
 		}
 
 		return []
